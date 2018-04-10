688d0de26c3323a3084c3f8cccf3cac830ffe62e
Weston <westonedwards@gmail.com>
2018-04-09 20:59:49 -0400

    Added route examples
    Switch used to serve 404 page when route not found.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 
-ReactDOM.render(<p>Placeholder text</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+    <div>Dashboard</div>
+);
+
+const AddExpensePage = () => (
+    <div>Add Page</div>
+);
+
+const EditExpensePage = () => (
+    <div>Edit Page</div>
+);
+
+const HelpPage = () => (
+    <div>Help Page</div>
+);
+
+const NotFoundPage = () => (
+    <div>404!</div>
+);
+
+const routes = (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={ExpenseDashboardPage} />
+            <Route path="/create" component={AddExpensePage} />
+            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage} />
+            <Route component={NotFoundPage} />
+        </Switch>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
