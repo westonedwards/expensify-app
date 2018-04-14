116ed83eef3abd63f311aceb98acf124891d9008
Weston <westonedwards@gmail.com>
2018-04-14 08:09:48 -0400

    Small test support for route params

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-    <div>Edit Page</div>
-);
-
+const EditExpensePage = (props) => {
+    return (
+        <div>Editing expense with id of {props.match.params.id}</div>
+    );
+};
 export default EditExpensePage;
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ const Header = () => (
         <h1>Expensify</h1>
         <NavLink exact to="/" activeClassName="is-active">Home</NavLink>
         <NavLink to="/create" activeClassName="is-active">Create</NavLink>
-        <NavLink to="/edit" activeClassName="is-active">Edit</NavLink>
         <NavLink to="/help" activeClassName="is-active">Help</NavLink>
     </header>
 );
